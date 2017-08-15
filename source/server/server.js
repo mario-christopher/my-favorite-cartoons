9199bb876809f1b25c24335ec153d098dfcfc4b5
Mario Christopher <mario@marioac.com>
2017-08-15 17:40:18 -0400

    favicon update

--- source/server/server.js
+++ source/server/server.js
@@ -2,6 +2,7 @@ import "babel-polyfill";
 import express from 'express';
 import cors from 'cors';
 import path from 'path';
+import favicon from 'serve-favicon';
 
 import { router as apiRouter } from './api/api';
 import { logUrl } from './middleware/log-url';
@@ -11,6 +12,9 @@ const PORT = process.env.PORT || 3000;
 
 let staticPath = path.resolve(__dirname, '../../');
 app.use(express.static(staticPath));
+
+let staticPathPublic = path.resolve(__dirname, '../../public');
+app.use(favicon(path.resolve(staticPathPublic, 'favicon.ico')));
 
 let staticPathBuild = path.resolve(__dirname, '../../public/build');
 app.use(express.static(staticPathBuild));
